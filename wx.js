fe4c724063b265ddb0d038226ff0df48e4ecd754
曾晋哲 <njzjz@qq.com>
2018-06-06 15:07:23 +0800

    Update wx.js

--- wx.js
+++ wx.js
@@ -18,7 +18,7 @@ if (useragent.match(/MicroMessenger/i) != 'MicroMessenger') {
 	// 这里警告框会阻塞当前页面继续加载
 	//alert('已禁止本次访问：您必须使用微信内置浏览器访问本页面！');
 	// 以下代码是用javascript强行关闭当前页面
-	var opened = window.open('https://wx.zgchemicals.mobi/login.php?url='+encodeURI(window.location.href), '_self');
+	var opened = window.open('https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa5ff24073b976f78e');
 	opened.opener = null;
 	//opened.close();
 }
